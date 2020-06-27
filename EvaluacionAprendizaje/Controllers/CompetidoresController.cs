95b0957852a96e739d0c520ed91b62bb74dfaf5e
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-06-27 04:05:51 -0300

    Agrego la lista de competidores

---
+++ EvaluacionAprendizaje/Controllers/CompetidoresController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EvaluacionAprendizaje.Models;
+using EvaluacionAprendizaje.Services;
+
+namespace EvaluacionAprendizaje.Controllers
+{
+    public class CompetidoresController : Controller
+    {
+        evaluacion2Entities context;
+        public CompetidoresController()
+        {
+            context = new evaluacion2Entities();
+        }
+
+        // GET: Competidores
+        public ActionResult Index()
+        {
+            return RedirectToAction("ListaCompetidores");
+        }
+
+        public ActionResult ListaCompetidores()
+        {
+            CompetidoresService competidoresService = new CompetidoresService(context);
+
+            List<Competidor> competidores = competidoresService.ObtenerTodos();
+
+            ViewBag.competidores = competidores;
+
+            return View();
+        }
+    }
+}
---
+++ EvaluacionAprendizaje/Services/CompetidoresService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using EvaluacionAprendizaje.Models;
+
+namespace EvaluacionAprendizaje.Services
+{
+    public class CompetidoresService
+    {
+        evaluacion2Entities context;
+        public CompetidoresService(evaluacion2Entities context)
+        {
+            this.context = context;
+        }
+
+        public List<Competidor> ObtenerTodos()
+        {
+            List<Competidor> competidores = context.Competidor.ToList(); ;
+            return competidores;
+        }
+    }
+}
