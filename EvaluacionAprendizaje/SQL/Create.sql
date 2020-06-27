51ea03ebe78ec80f840632bca6330279438c5ae0
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-06-27 05:54:08 -0300

    Agrego la property Identity a los id en la DB

--- EvaluacionAprendizaje/SQL/Create.sql
+++ EvaluacionAprendizaje/SQL/Create.sql
@@ -2,13 +2,15 @@ use evaluacion2
 go
 
 create table Competidor(
-	id int Primary Key,
+	id int Primary Key identity,
 	nombre varchar(30) not null
 )
 go
 
+
+
 create table PremiosCompetidor(
-	id int Primary Key,
+	id int Primary Key identity,
 	cantidadPremios int not null,
 	anio int not null,
 	idCompetidor int Foreign Key References Competidor(id)
--- EvaluacionAprendizaje/SQL/Inserts.sql
+++ EvaluacionAprendizaje/SQL/Inserts.sql
@@ -1,17 +1,17 @@
 use evaluacion2
 go
 
-insert into Competidor(id, Nombre)
-		values	(1, 'Pepe'),
-				(2, 'Ana'),
-				(3, 'Dario'),
-				(4, 'Ezequiel');
+insert into Competidor(Nombre)
+		values	('Pepe'),
+				('Ana'),
+				('Dario'),
+				('Ezequiel');
 go
 
 
-insert into PremiosCompetidor(id, cantidadPremios, anio, idCompetidor)
-		values	(1, 3, 2018, 1),
-				(2, 1, 2019, 1),
-				(3, 4, 2018, 2),
-				(4, 1, 2020, 3);
+insert into PremiosCompetidor(cantidadPremios, anio, idCompetidor)
+		values	(3, 2018, 1),
+				(1, 2019, 1),
+				(4, 2018, 2),
+				(1, 2020, 3);
 go
